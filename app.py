6d74a2d6ed5defe765010b706a0258fc7390ad96
chatytrio <chatytrio@gmail.com>
2025-06-11 10:40:00 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 import math
 from datetime import datetime, timedelta
 
+# API KEY de OpenRouteService
 api_key = "5b3ce3597851110001cf6248e38c54a14f3b4a1b85d665c9694e9874"
 client = openrouteservice.Client(key=api_key)
 
